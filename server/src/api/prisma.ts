ee515b6716fa97c41ca7e8751bcdc29a05ecf2ee
lwarden1 <lwarden1@trinity.edu>
2023-03-05 21:48:40 -0600

    fix raw prisma api

--- server/src/api/prisma.ts
+++ server/src/api/prisma.ts
@@ -1,26 +1,32 @@
 import { Router, Request, Response, NextFunction } from "express";
 import prisma from "../prisma";
-import { validateUser } from '../utils';
+import { parseJson, validateUser } from '../utils';
 
 const router = Router();
 
-router.all('/:model/:op', [validateUser, (req: Request, res: Response, next: NextFunction) => {
+router.all('/:model/:op', [parseJson, validateUser, (req: Request, res: Response, next: NextFunction) => {
     if (!req.session!.user?.isTeacher) {
         res.sendStatus(401);
     } else {
         next()
     }
 }], async (req: Request, res: Response) => {
-    if (req.params.model in ['class', 'semester', 'user']) {
-        // @ts-ignore
-        const model = prisma[req.params.model];
-        if (req.params.op in model) {
+    console.log(`[${req.originalUrl}]: Prisma ${req.params.model}.${req.params.op}(${JSON.stringify(req.body)})`)
+    try {
+        if (['class', 'semester', 'user'].includes(req.params.model)) {
             // @ts-ignore
-            res.json(await model[req.params.op](req.body));
-        } else {
-            res.sendStatus(404)
+            const model = prisma[req.params.model];
+            if (req.params.op in model) {
+                const op = model[req.params.op];
+                // @ts-ignore
+                return res.json(await op(req.body));
+            }
         }
+    } catch (e) {
+        console.log(e)
+        return res.sendStatus(500);
     }
+    res.sendStatus(404)
 })
 
 export default router;
